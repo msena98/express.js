ff966faa3db598170ffedbe612ee32127ddd509a
Matheus Sena <msena2103@gmail.com>
2022-11-03 21:12:55 -0300

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,15 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('test', 'root', 'Suporte$ena2103@g', {
     host: 'localhost',
     dialect: 'mysql'
-});
+})
 
+const Postagem = sequelize.define('postagens', {
+    titulo:{
+        type: Sequelize.STRING
+    },
+    conteudo:{
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.sync({force: true});
