5898a2c14927bd7df143c2b3a77d4d797ada20e9
Matheus Sena <msena2103@gmail.com>
2022-11-05 15:42:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Post = require('./models/Post')
 
 // Config
     // Template Engine
@@ -16,7 +17,14 @@ const bodyParser = require('body-parser')
     })
 
     app.post('/add', function(req,res){
-        res.send(`Titulo: ${req.body.titulo} Conteudo: ${req.body.conteudo}`)
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.send("Post criado com sucesso!")
+        }).catch(function(erro){
+            res.send(`Houve um erro: ${erro}`)
+        })
     })
 
 app.listen(8081, function(){
