13f5eb0c3a440d444ccb36351e89058c8125aff6
Matheus Sena <msena2103@gmail.com>
2022-11-05 18:16:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ const Post = require('./models/Post')
         })
     })
 
+    app.get('/deletar/:id', function(req,res){
+        Post.destroy({where: {'id': req.params.id}}).then(function(req,res){
+            res.send('Postagem deletada com sucesso!')
+        }).catch(function(erro){
+            res.send('Esta postagem não existe')
+        })
+    })
+
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
 });
