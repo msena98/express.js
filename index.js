c555aa54858bb6622a2eb390c9a15e69c48c16ac
Matheus Sena <msena2103@gmail.com>
2022-11-05 15:16:37 -0300

    Require body-parser

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const app = express();
-const handlebars = require('express-handlebars');
-const Sequelize = require('sequelize');
+const express = require("express")
+const app = express()
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
 
 // Config
     // Template Engine
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json());
     // Conexão com banco de dados MySql
         const sequelize = new Sequelize('test', 'root', 'Suporte$ena2103@', {
         host: 'localhost',
