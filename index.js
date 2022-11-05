e37e7da3c583b86b15f37a96c9a6ebc719e9d1d0
Matheus Sena <msena2103@gmail.com>
2022-11-05 13:44:52 -0300

    Config template engine

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars');
+
+// Config
+  // Template Engine
+    app.engine('handlebars', handlebars({defaultLayout: main}))
+    app.set('view engine', 'handlebars')
 
 
 app.listen(8081, function(){
