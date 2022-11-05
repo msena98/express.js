4fc044692be2f22b6b565a829cfbd2f4b2eeb519
Matheus Sena <msena2103@gmail.com>
2022-11-05 15:30:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
 
 // Config
     // Template Engine
@@ -11,11 +10,6 @@ const Sequelize = require('sequelize')
     //Body Parser
         app.use(bodyParser.urlencoded({extended: false}))
         app.use(bodyParser.json());
-    // Conexão com banco de dados MySql
-        const sequelize = new Sequelize('test', 'root', 'Suporte$ena2103@', {
-        host: 'localhost',
-        dialect: 'mysql'
-        }) 
 // Rotas
     app.get('/cad', function(req,res){
         res.render('formulario')
