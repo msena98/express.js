44cdf43c51bef5cd45244e3d0ed227cd622a0250
Matheus Sena <msena2103@gmail.com>
2022-11-05 13:56:43 -0300

    Created first route

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('test', 'root', 'Suporte$ena2103@', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
 
 // Config
-  // Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: main}))
-    app.set('view engine', 'handlebars')
-
+    // Template Engine
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexão com banco de dados MySql
+        const sequelize = new Sequelize('test', 'root', 'Suporte$ena2103@', {
+        host: 'localhost',
+        dialect: 'mysql'
+        }) 
+// Rotas
+    app.get('/cad', function(req,res){
+        res.send('Rota de cadastros de POSTS');
+    })
 
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
